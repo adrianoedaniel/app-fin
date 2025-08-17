d47d965122165e2a4bec2f5918e9bced2e45743a
Adriano Elias Daniel <38328964+adrianoedaniel@users.noreply.github.com>
2025-08-17 15:21:56 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: '/financeiro-app/',
+  base: '/app-fin/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
